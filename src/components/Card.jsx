75e9ff58f31b9383073f2468902839f654707064
aybike <aybikesayar@gmail.com>
2022-10-15 12:31:16 +0300

    first

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className="card">
+            <div className="image">
+                <img src="" alt="" />
+            </div>
+            <div className="title">
+                <p className="name"></p>
+                <p className="mail"></p>
+                <p className="age"></p>
+            </div>
+        </div>
+    )
+}
+
+export default Card
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="head">
+            <h1>Employee List</h1>
+            <p>(Employees 1 to 5)</p>
+        </div>
+    )
+}
+
+export default Header
